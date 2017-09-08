a46b9281e13a5014cc6139b1ac4094d03c19c60c
DESKTOP-BUUECPU\wada <g1527079@st.t-kougei.ac.jp>
2017-09-08 16:50:20 +0900

    四角形のテクスチャをただしく表示

--- Win32Project1/Win32Project1/Modelclass.cpp
+++ Win32Project1/Win32Project1/Modelclass.cpp
@@ -94,19 +94,19 @@ bool ModelClass::InitializeBuffers(ID3D11Device*device)
 
 	//ポイントを描画するには、時計回りの順序でポイントを作成します。
 	vertices[0].position = XMFLOAT3(-1.0f, 1.0f, 0.0f);
-	vertices[0].texture = XMFLOAT2(0.0f, 1.0f);
+	vertices[0].texture = XMFLOAT2(0.0f, 0.0f);
 
 	vertices[1].position = XMFLOAT3(1.0f, -1.0f, 0.0f);
-	vertices[1].texture = XMFLOAT2(0.5f, 0.0f);
+	vertices[1].texture = XMFLOAT2(1.0f, 1.0f);
 
 	vertices[2].position = XMFLOAT3(-1.0f, -1.0f, 0.0f);
-	vertices[2].texture = XMFLOAT2( 1.0f, 1.0f);
+	vertices[2].texture = XMFLOAT2( 0.0f, 1.0f);
 
 	vertices[3].position = XMFLOAT3(-1.0f, 1.0f, 0.0f);
-	vertices[3].texture = XMFLOAT2(0.0f, 1.0f);
+	vertices[3].texture = XMFLOAT2(0.0f, 0.0f);
 
 	vertices[4].position = XMFLOAT3(1.0f, 1.0f, 0.0f);
-	vertices[4].texture = XMFLOAT2(0.5f, 0.0f);
+	vertices[4].texture = XMFLOAT2(1.0f, 0.0f);
 
 	vertices[5].position = XMFLOAT3(1.0f, -1.0f, 0.0f);
 	vertices[5].texture = XMFLOAT2(1.0f, 1.0f);
